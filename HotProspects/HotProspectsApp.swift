a8102572718e65c980489fbbd6b779d0de77e201
Sergey Zakharov <zakharov.nsg@gmail.com>
2026-04-17 03:43:40 +0500

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Сергей Захаров on 17.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
